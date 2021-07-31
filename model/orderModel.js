dc27defbc645f6c755742eb27264adacf5d61e0d
swapno5896 <swapno5896@yahoo.com>
2021-07-31 20:02:59 +0600

    order model is ready to use

--- model/orderModel.js
+++ model/orderModel.js
@@ -2,13 +2,11 @@ const {Schema,model} = require('mongoose');
 
 const orderSchema = new Schema({
     userId : Schema.Types.ObjectId,
-    customer:{
-        delevaryAddress:{type:String,require:[true,`Order can't complete without delevary address`]},
-        phone:{type:String,require:[true,`Please insert phone number for further communication`]},
-        paymentType:{type:String, require:true}
-    },
-    price:Number,
-    orderTime:{type:String, default:Date.now}
+    customerId:Schema.Types.ObjectId,
+    quantity:Number,
+    date: Date,
+    status:String,
+  
 })
 
 module.exports = model('orders',orderSchema)
--- model/userModel.js
+++ model/userModel.js
@@ -4,7 +4,8 @@ const usersSchema = new Schema({
     email:{type:String,require:[true,'Please insert your email']},
     phone:{type:String,require:[true,'Please insert your phone']},
     address:{type:String,require:[true,'Please insert your address']},
-    location:{latitude:String,longitude:String}
+    location:{latitude:String,longitude:String},
+    pamentMethod:String,
 });
 
 module.exports = model('users',usersSchema);
