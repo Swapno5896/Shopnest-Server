1e19e1413068f5458ef5e3cb42ee049e51a96f66
swapno5896 <swapno5896@yahoo.com>
2021-07-31 20:00:00 +0600

    celler model is ready

--- model/cellerModel.js
+++ model/cellerModel.js
@@ -1,4 +1,11 @@
 const {Schema,model} = require('mongoose');
 const cellerSchema = new Schema({
-    
-})
+    name:{type:String,require:[true,'Please insert your name']},
+    adminEmail:{type:String,require:[true,'Please insert your email']},
+    collaborator:{id:String,isAccepted:Boolean},
+    phone:{type:String,require:[true,'Please insert your phone']},
+    address:{type:String,require:[true,'Please insert your address']},
+    location:{latitude:String,longitude:String}
+});
+
+module.exports = model('cellers',cellerSchema);
